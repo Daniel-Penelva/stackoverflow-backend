f9ba11993b5ace0ff1d934827d46d07c0c5193de
Daniel Penelva <d4n.andrade@gmail.com>
2024-11-11 20:19:40 -0300

    feat: criando projeto springboot para backend

---
+++ src/main/java/com/api/stackoverflow_backend/StackoverflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.stackoverflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/stackoverflow_backend/StackoverflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.stackoverflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
