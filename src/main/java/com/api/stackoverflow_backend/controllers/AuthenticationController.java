81fcea7d95eff78d609ecf600838834d5b81c60d
Daniel Penelva <d4n.andrade@gmail.com>
2024-12-02 21:22:27 -0300

    feat: alterando o método createAuthenticationToken da classe AuthenticationController para retornar o id do usuário e acessar o cabeçalho
    Essa alteração tem como objetivo retornar o id do usuário e garantir a compatibilidade do CORS, ou seja, permite que o cliente acesse o cabeçalho Authorization.
    
    Foram add as constantes de Cabeçalho (onde define o nome do cabeçalho onde o token será enviado no lado do cliente. O cliente precisa incluir isso em solicitações autenticadas.) e Prefixo de Token (onde define o prefixo Bearer para o token JWT.)
    
    Add uma lógica para Capturar o ID do Usuário com base no email (username). Após carregar os detalhes do usuário (UserDetails), é usado o repositório para buscar a entidade do usuário no banco, garantindo acesso a propriedades como o ID. O uso de Optional protege contra valores null e ajuda a evitar exceções de NullPointerException.
    
    Vale ressaltar que a variavel jwt gera o token JWT usando o método generateToken, com base no username (email) do usuário. Esse token será usado para autenticação em requisições subsequentes.
    
    Add um condicionante retornando o userId no corpo da resposta, ou seja, se o usuário for encontrado no banco, o ID é retornado como parte da resposta. Isso é útil para que o cliente saiba o identificador único do usuário (por exemplo, para consultas futuras ou ações no sistema). E o formato da resposta é um objeto JSON contendo apenas o userId.
    
    Add as exposição dos cabeçalhos no CORS, por exemplo, o "Access-Control-Expose-Headers" permite que o cabeçalho Authorization seja acessível no lado do cliente. Isso é necessário porque, por padrão, o navegador bloqueia cabeçalhos customizados e o "Access-Control-Allow-Headers" que especifica os cabeçalhos permitidos em requisições CORS.
    
    Add o token no cabeçalho "response.addHeader(HEADER_STRING, TOKEN_PREFIX + jwt);" onde o token JWT gerado é incluído no cabeçalho Authorization, usando o prefixo Bearer.

--- src/main/java/com/api/stackoverflow_backend/controllers/AuthenticationController.java
+++ src/main/java/com/api/stackoverflow_backend/controllers/AuthenticationController.java
@@ -1,5 +1,10 @@
 package com.api.stackoverflow_backend.controllers;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -11,8 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.stackoverflow_backend.dtos.AuthenticationRequest;
-import com.api.stackoverflow_backend.dtos.AuthenticationResponse;
+import com.api.stackoverflow_backend.entities.User;
+import com.api.stackoverflow_backend.repository.UserRepository;
 import com.api.stackoverflow_backend.utils.JwtUtil;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class AuthenticationController {
@@ -24,11 +32,17 @@ public class AuthenticationController {
     private UserDetailsService userDetailsService;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private JwtUtil jwtUtil;
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 
     // http://localhost:8080/authentication
     @PostMapping("/authentication")
-    public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
+    public void createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse response) throws IOException, JSONException {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
         } catch (BadCredentialsException e) {
@@ -36,9 +50,44 @@ public class AuthenticationController {
         }
 
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
+
+        Optional<User> optionalUser = userRepository.findByEmail(userDetails.getUsername());
+
         final String jwt = jwtUtil.generateToken(userDetails.getUsername());
 
-        return new AuthenticationResponse(jwt);
+        if (optionalUser.isPresent()) {
+            response.getWriter().write(new JSONObject()
+                    .put("userId", optionalUser.get().getId())
+                    .toString());
+        }
+
+        response.addHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Allow-Headers",
+                "Authorization," + " X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept, X-Custom-header");
+
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX + jwt);
+
     }
     
 }
+
+
+/** OBSERVAÇÃO.
+ * 
+ * Fluxo do método createAuthenticationToken:
+ *  1. Autenticação:
+ *      -> O authenticationManager valida o email e a senha enviados.
+ * 
+ *  2. Carregamento do usuário:
+ *      -> O serviço de detalhes do usuário (UserDetailsService) carrega as informações do usuário.
+ * 
+ *  3. Busca no banco:
+ *      -> O repositório busca o usuário completo para obter o ID.
+ * 
+ *  4. Geração de token: 
+ *      -> Um token JWT é criado com o username.
+ * 
+ *  5. Resposta ao Cliente:
+ *      -> O ID do usuário é enviado no corpo da resposta como JSON.
+ *      -> O token JWT é incluído nos cabeçalhos.
+*/
